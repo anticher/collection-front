eaa2116d0b7baa7c488b4caba4bffbb111cbbce7
anticher <anticherq@gmail.com>
2022-10-08 21:23:30 +0300

    feat: add localiztion toggling

--- src/components/Header/set-localization-button/Set-localization-button.tsx
+++ src/components/Header/set-localization-button/Set-localization-button.tsx
@@ -2,15 +2,31 @@ import styles from "./Set-localization-button.module.css";
 import { useState } from "react";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import ToggleButton from "react-bootstrap/ToggleButton";
+import { useAppDispatch, useAppSelector } from "../../../app/hooks";
+import { useTranslation } from 'react-i18next'
+import { setLocalization } from "../../../app/slices/settings.slice";
+import { useBeforeunload } from 'react-beforeunload';
 
 function SetLocalizationButton() {
-  // const [checked, setChecked] = useState(false);
-  const [localRadioValue, setLocalRadioValue] = useState("1");
-
+  const dispatch = useAppDispatch();
+  const appLocalization = useAppSelector((state) => state.settings.localization);
+  const { i18n } = useTranslation()
+  const [localRadioValue, setLocalRadioValue] = useState(appLocalization);
+  
   const localRadios = [
-    { name: "En", value: "1" },
-    { name: "Ru", value: "2" },
+    { name: "en", value: "en" },
+    { name: "ru", value: "ru" },
   ];
+
+  useBeforeunload(() => localStorage.setItem("localization", appLocalization))
+
+  const onClickHandler = (e: React.ChangeEvent<EventTarget>) => {
+    let target = e.target as HTMLInputElement;
+    i18n.changeLanguage(target.value)
+    dispatch(setLocalization(target.value));
+    setLocalRadioValue(target.value);
+  };
+
   return (
     <>
       <ButtonGroup>
@@ -24,7 +40,7 @@ function SetLocalizationButton() {
             name="local-radio"
             value={radio.value}
             checked={localRadioValue === radio.value}
-            onChange={(e) => setLocalRadioValue(e.currentTarget.value)}
+            onChange={(e) => onClickHandler(e)}
           >
             {radio.name}
           </ToggleButton>
